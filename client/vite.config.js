71e6ce9dbaeb78d13bac3844c041842ce954953b
nandyamit <nandyamit.in@gmail.com>
2025-01-12 15:53:48 -0800

    code ready for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -13,14 +13,5 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
   }
-})
+});
